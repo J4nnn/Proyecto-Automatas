9dedb009e495bae38f681fdff5b856e6714db275
Janer Vega <janervega2210048@correo.uis.edu.co>
2023-07-07 12:18:54 -0500

    Funciones que se usarán

---
+++ funciones.py
@@ -0,0 +1,15 @@
+import string
+
+# Convertir de cadena de texto a lista
+def convertir(texto):
+    lista = list(texto.split(' '))
+    return lista
+
+# Leer un archivo de texto
+def leer():
+    texto = open('ejemplos/', 'r', encoding='utf-8')
+    contenido = texto.read()
+    texto.close()
+    return contenido # Devolvemos el contenido en forma de cadena
+
+# Eliminamos los simbolos de puntuación del texto
