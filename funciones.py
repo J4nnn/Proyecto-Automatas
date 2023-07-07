2c26bd7a2f7b75fb5dc9efff3bcf16d2f3db147f
Janer Vega <janervega2210048@correo.uis.edu.co>
2023-07-07 12:46:47 -0500

    Corregido (leer)

--- funciones.py
+++ funciones.py
@@ -1,4 +1,6 @@
 import string
+import unidecode # Se necesita instalar -> 'pip install Unidecode'
+import re
 
 # Convertir de cadena de texto a lista
 def convertir(texto):
@@ -7,12 +9,15 @@ def convertir(texto):
 
 # Leer un archivo de texto
 def leer(archivo):
-    texto = open('ejemplos/{}'.format(archivo), 'r', encoding='utf-8')
+    texto = open('ejemplos/{}.txt'.format(archivo), 'r', encoding='utf-8')
     contenido = texto.read()
     texto.close()
     return contenido # Devolvemos el contenido en forma de cadena
 
-# Eliminamos los simbolos de puntuación del texto
+# Eliminar los simbolos de puntuación y acentos del texto
 def eliminarPuntuacion(texto):
-    texto = texto.translate(str.maketrans('', '', string.punctuation))
+    texto = texto.translate(str.maketrans('', '', string.punctuation)) # Eliminamos puntuación
+    texto = unidecode.unidecode(texto) # Eliminamos acentos
+    # Notar que ¿ y ¡ son casos especiales del español y no se eliminan arriba
+    texto = re.sub('[¿¡]', '', texto) # Eliminamos ¿ y ¡
     return texto
