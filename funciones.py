ffd522a4dc99db78e196ae51da139f6834a0b09f
Janer Vega <janervega2210048@correo.uis.edu.co>
2023-07-07 14:51:47 -0500

    Agregadas dos funciones)
    eliminarCadenasVacias y eliminarSaltosDeLinea

--- funciones.py
+++ funciones.py
@@ -3,12 +3,12 @@ import unidecode # Se necesita instalar -> 'pip install Unidecode'
 import re
 
 # Convertir de cadena de texto a lista
-def convertir(texto):
+def convertirALista(texto):
     lista = list(texto.split(' '))
     return lista
 
 # Leer un archivo de texto
-def leer(archivo):
+def leerArchivo(archivo):
     texto = open('ejemplos/{}.txt'.format(archivo), 'r', encoding='utf-8')
     contenido = texto.read()
     texto.close()
@@ -20,7 +20,9 @@ def eliminarPuntuacion(texto):
     texto = unidecode.unidecode(texto) # Eliminamos acentos
     # texto = unidecode.unidecode(texto) # Eliminamos ? y ! que reemplazaron a ¿ y ¡ en la 1ra ejecución
     # Notar que ¿ y ¡ son casos especiales del español y no se eliminan arriba
-    # Nota: En donde se elimina la puntuación, lo que se hace es que se cambian: ¿->? y ¡->!, entonces eliminar ¿ y ¡ no tiene sentido. Hay dos posibles soluciones, eliminar ? y ! o correr una segunda vez la función de eliminar puntuación.
+    # Nota: En donde se elimina la puntuación, lo que se hace es que se cambian: ¿->? y ¡->!,
+    # entonces eliminar ¿ y ¡ no tiene sentido. Hay dos posibles soluciones, eliminar ? y !
+    # o correr una segunda vez la función de eliminar puntuación.
     # Nota de la nota: La 2da solución no funciona
     texto = re.sub('[?!]', '', texto) # Eliminamos ¿ y ¡
     return texto
@@ -28,3 +30,15 @@ def eliminarPuntuacion(texto):
 def pasarAMinusculas(texto):
     texto = texto.lower()
     return texto
+
+# Elimar saltos de linea y los reemplaza por un ' '
+# Nota: esto nos deja dobles espacios y por ende, cadenas vacias en la lista luego de convertir
+def eliminarSaltosDeLinea(texto):
+    texto = ' '.join(texto.split('\n'))
+    return texto
+
+# Eliminar las cadenas vacias que quedan luego de 
+def eliminarCadenasVacias(lista):
+    while('' in lista):
+        lista.remove('')
+    return lista
